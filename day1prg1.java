8feace9b627470cbd3bc1e1e14e5e5345feb3605
annikumar07 <72306521+annikumar07@users.noreply.github.com>
2024-10-03 13:02:20 +0530

    Add files via upload

---
+++ day1prg1.java
@@ -0,0 +1,12 @@
+class day1prg1{
+	public static void main(String[] args)
+	{
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		{
+			System.out.println(a+" is even");
+		}
+		else
+			System.out.println(a+" is odd");
+	}
+}
