4334f592fa0a358919ba9766ccd544751aae9d83
annikumar07 <72306521+annikumar07@users.noreply.github.com>
2024-10-03 13:41:12 +0530

    Add files via upload

---
+++ day1prg5.java
@@ -0,0 +1,16 @@
+class day1prg5{
+	public static void main(String[] args)
+	{
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+		if (a>b && a>c)
+			System.out.println(a+" is greatest");
+		else if(b>c)
+			System.out.println(b+" is greatest");
+		else
+			System.out.println(c+" is greatest");
+		
+	}
+}
